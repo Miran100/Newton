e666415150605e36fea0864e930becd2172815be
Miran100 <1meran2slemany3@gmail.com>
2025-04-26 15:20:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
 import telegram
 
-TOKEN = "YOUR_BOT_TOKEN_HERE"
+TOKEN = "7565234591:AAGMwIH1KHcxIcvtUoCYfH-HT5U30-95NNw"
 bot = telegram.Bot(token=TOKEN)
 
 app = Flask(__name__)
